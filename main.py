6e05adf826def3e91c1411956cae02934f942f4e
TRMT-Yuka <momo144.art@gmail.com>
2022-05-17 11:23:06 +0900

    add Buddha bot and sbert dir

--- main.py
+++ main.py
@@ -12,6 +12,38 @@ from linebot.models import (
 )
 import os
 
+
+
+#Buddha-Bot
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+
+sbert = SentenceTransformer('Buddha-Bot/sbert_stair')
+print("sbert OK!")
+
+def similarity(id1, id2):
+    return cosine_similarity([vectors[id2idx[id1]]], [vectors[id2idx[id2]]])[0][0]
+
+with open('Buddha-Bot/Butta_dict.binaryfile', 'rb') as bf:
+    Butta_dict = pickle.load(bf)
+    
+with open('Buddha-Bot/Yahoo_dict.binaryfile', 'rb') as yf:
+    Yahoo_dict = pickle.load(yf)
+
+
+def Origin(input_Q):
+    return input_Q+"momomo"
+
+#質問を入力すると回答が表示されます
+def Buddha(input_Q):
+    Buddha_cos = {}
+    for key in Butta_dict:
+        Buddha_cos[key] = cosine_similarity([Butta_dict[key]],[sbert.encode(input_Q)])
+        
+    max_k = max(Buddha_cos, key=Buddha_cos.get)
+    return max_k
+
+
 app = Flask(__name__)
 
 #環境変数取得
@@ -43,7 +75,10 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=Origin(event.message.text)))
+
+
+
 
 
 if __name__ == "__main__":
